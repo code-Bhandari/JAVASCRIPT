84be6edecaba6bf018bd8afa4ec4b51d2e594572
Shishant-Bhandari <90854755+code-Bhandari@users.noreply.github.com>
2023-11-30 05:23:44 +0000

    addup incredecrement

--- basics_01/conversionoperation.js
+++ basics_01/conversionoperation.js
@@ -21,3 +21,37 @@ let sumnumber = 33
 let stringnumber = String(sumnumber)
 console.log(stringnumber);
 console.log(typeof stringnumber)
+
+
+
+///// opertaion ///
+let v1 = 3
+let nev1 = -v1
+console.log(nev1)
+
+let str1 = "yes"
+let str2 = "  sir"
+let str3 =  str1 + str2;
+console.log(str3);
+
+console.log(2+2+"3")  
+// 43 
+console.log("2"+2+"3")
+// 223 
+console.log("2"+2+3) 
+//  223
+
+//***********increment/decrement***********
+
+let c = 20;
+let d = c++;
+
+console.log(`c:${c},d:${d}`); // 21 , 20 
+let e = --c;
+console.log(`e:${c},d:${d}`); // 20 , 20
+let f = e++ + --d;
+console.log(`f:${f++},d:${--d}`); //39, 18
+let s = --f;
+console.log(`c:${s},s:${--d}`); //39 , 17 
+let p = --s
+console.log(`p:${p++},d:${--d}`); // 38 , 16
