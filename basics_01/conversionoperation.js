101dbdf5c474cebc51543c887acb6101c3700026
Shishant-Bhandari <90854755+code-Bhandari@users.noreply.github.com>
2023-11-30 04:52:02 +0000

    conversion of operation

---
+++ basics_01/conversionoperation.js
@@ -0,0 +1,23 @@
+let score = "33abc";
+let run = null
+let play = undefined
+//console.log(typeof score);
+let valueIn_number = Number(score);
+console.log(typeof valueIn_number);
+console.log(score)
+
+let valueIn_number2 = Number(run);
+console.log(typeof valueIn_number2);
+console.log(valueIn_number2);
+
+let valueIn_number3 = Number(run);
+console.log(typeof valueIn_number3);
+console.log(valueIn_number3);
+
+// "33" = 33
+// "33abc" = nan
+// true = 1,false = 0
+let sumnumber = 33
+let stringnumber = String(sumnumber)
+console.log(stringnumber);
+console.log(typeof stringnumber)
